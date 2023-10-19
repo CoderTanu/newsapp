9cf7c2c1afffcf8076fea6fd10c7a780d5bd9e24
Tanu Kumari <tk6353129@gmail.com>
2023-10-19 23:35:13 +0530

    NewApp
    created using reactjs  by taking reference from codewith harry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Bar from './components/Bar';
+import News from './components/News';
 
-function App() {
+export default class App extends Component{
+
+render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+<Bar/>
+<News/>
+
+  </div>
+)
+}
+  
 }
 
-export default App;
+
